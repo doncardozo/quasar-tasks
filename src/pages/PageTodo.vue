82ca752e2f9e8983705d86131a991dcf19f4d0a4
doncardozo <oa.cardozoz@gmail.com>
2020-05-13 13:13:44 -0300

    update

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -3,12 +3,30 @@
     <q-list
       separator
       borderer>
-      <Tasks 
-        v-for="(task, key) in tasks"
-        :key="key"
-        :task="task"
-        :id="key"
-      />
+
+      <div v-if="Object.keys(tasksTodo).length">
+        <q-banner dense class="bg-orange text-white text-center">
+          Todo tasks
+        </q-banner>      
+        <Tasks 
+          v-for="(task, key) in tasksTodo"
+          :key="key"
+          :task="task"
+          :id="key"
+        />
+      </div>
+
+      <div :class="Object.keys(tasksTodo).length ? 'q-mt-lg' : ''" v-if="Object.keys(tasksCompleted).length">
+        <q-banner dense class="bg-green text-white text-center">
+          Completed tasks
+        </q-banner>
+        <TasksCompleted
+          v-for="(task, key) in tasksCompleted"        
+          :key="key"
+          :task="task"
+          :id="key"
+        />      
+      </div>
       <div class="absolute-bottom text-center q-mb-lg">
         <q-btn 
           @click="showAddTask = true"
@@ -28,12 +46,14 @@
 <script>
 import { mapGetters } from 'vuex'
 import Tasks from 'components/Tasks/Tasks'
+import TasksCompleted from 'components/Tasks/TasksCompleted'
 import AddTask from 'components/Tasks/Modals/AddTask'
 
 export default {
   name: 'PageTodo',
   components: {
     Tasks,
+    TasksCompleted,
     AddTask
   },
   data(){
@@ -42,7 +62,7 @@ export default {
     }
   },
   computed: {
-    ...mapGetters('tasks', ['tasks'])
+    ...mapGetters('tasks', ['tasksTodo','tasksCompleted'])
   }
 }
 </script>
